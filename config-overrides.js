75bf1cd67c186d9d9fcc6bc173648b39f9bfece8
domzgarcia <dom.garcia@nuworks.ph>
2021-01-13 23:57:53 +0800

    cleaning up

--- config-overrides.js
+++ config-overrides.js
@@ -3,12 +3,8 @@ module.exports = function override(config, env) {
     config.resolve.alias['Layouts'] = path.join(__dirname, 'src', 'layouts');
     config.resolve.alias['Pages'] = path.join(__dirname, 'src', 'pages');
     config.resolve.alias['Utilities'] = path.join(__dirname, 'src', 'utilities');
-    
-    // config.resolve.alias['pages'] = path.join(__dirname, 'pages');
-    // config.resolve.alias['services'] = path.join(__dirname, 'services');
-    // config.resolve.alias['utils'] = path.join(__dirname, 'utils');
-    // config.resolve.alias['config'] = path.join(__dirname, 'config');
-    // config.resolve.alias['images'] = path.join(__dirname, 'public', 'images'); 
-    // I have yet to try that.   
+    config.resolve.alias['Services'] = path.join(__dirname, 'src', 'services');
+    config.resolve.alias['DTO'] = path.join(__dirname, 'src', 'dto');
+    config.resolve.alias['Images'] = path.join(__dirname, 'public', 'images');
     return config;
 }
--- src/layouts/app.main.js
+++ src/layouts/app.main.js
@@ -5,31 +5,18 @@ import asyncRoute from 'Utilities/asyncRoute';
 const HeaderComponent = asyncRoute(() => import('Layouts/header.component'));
 const FooterComponent = asyncRoute(() => import('Layouts/footer.component'));
 const Home    = asyncRoute(() => import('Pages/home'));
-const About   = asyncRoute(() => import('Pages/about'));
-const Contact = asyncRoute(() => import('Pages/contact'));
-const DocsRoute    = asyncRoute(() => import('Pages/docs'));
 
 class App extends Component {
     constructor(props){
         super(props);
     }
 
-    randomizer(){
-        const rand = Math.floor(Math.random() * 9999) + 101;
-        return true ? 'disabled' : rand;
-    }
-
     render() {
         return (
-            <div className={`App-${this.randomizer()}`}>
+            <div className="App">
                 <HeaderComponent />
                 <Switch>
                     <Route exact path='/' component={Home}/>
-                    <Route exact path='/about' component={About}/>
-                    <Route exact path='/contact-us' component={Contact}/>
-                    
-                    {/* Nested */}
-                    <Route path='/docs' component={DocsRoute}/>
                 </Switch>
                 <FooterComponent />
             </div>
